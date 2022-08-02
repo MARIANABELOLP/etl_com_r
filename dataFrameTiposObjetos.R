25df6b7b98ee8a88167e2b891cda6115f459290e
MARIANABELOLP <110028284+MARIANABELOLP@users.noreply.github.com>
2022-08-02 09:06:44 -0300

    Atividade Tipos de Objetos - Data frame

---
+++ dataFrameTiposObjetos.R
@@ -0,0 +1,16 @@
+#nomes dos animais
+nomeCão <- c("Bela", "Mel", "Patty", "Suzy", "Telo")
+
+#sexo dos animais
+  sexoCão <- c("fêmea", "fêmea", "fêmea", "fêmea", "macho")
+  
+#raça dos animais
+  raçaCão <- c("srd", "basset", "west", "west", "west")
+  
+#data frame dos animais
+  listaCão <- data.frame(
+    nome = nomeCão,
+    sexo = sexoCão,
+    raça = raçaCão
+  )
+listaCão
